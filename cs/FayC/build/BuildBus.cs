31b85117ca57e545d4ea442dea59e1793b5d3c2b
Larlf.Wang <larlf.wang@gmail.com>
2018-09-21 16:34:40 +0800

    Add lost files

---
+++ cs/FayC/build/BuildBus.cs
@@ -0,0 +1,14 @@
+﻿
+
+using fay.compiler.runtime;
+
+namespace fay.compiler.Build
+{
+	/// <summary>
+	/// 编译时的全局对象
+	/// </summary>
+	public static class BuildBus
+	{
+		public static FayDomain Domain = new FayDomain();
+    }
+}
---
+++ cs/FayC/build/CodeBuilder.cs
@@ -0,0 +1,81 @@
+﻿using fay.compiler.runtime;
+using fay.lib.Instruction;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace fay.compiler
+{
+	/// <summary>
+	/// 代码生成器
+	/// </summary>
+	public class CodeBuilder
+	{
+		FayLib _lib;
+		FayClass _class;
+		FayFun _fun;
+		FayScope _scope;
+
+		List<FayInst> insts = new List<FayInst>();
+
+		public FayLib Lib => this._lib;
+
+		//_________________________________________________________
+
+		public CodeBuilder()
+		{
+
+		}
+
+		public void bindLib(FayLib lib)
+		{
+			this._lib = lib;
+		}
+
+		public void bindClass(FayClass clazz)
+		{
+			this._lib.addClass(clazz);
+			this._class = clazz;
+		}
+
+		public void bindFun(FayFun fun)
+		{
+			this._class.addFun(fun);
+			this._fun = fun;
+		}
+
+		public void addScope()
+		{
+			this._scope = new FayScope(this._scope);
+		}
+
+		public void subScope()
+		{
+
+		}
+
+		public void AddInst(FayInst inst)
+		{
+			this.insts.Add(inst);
+		}
+
+		public List<FayInst> Build()
+		{
+			return this.insts;
+		}
+
+		public string ToString(string index)
+		{
+			StringBuilder sb = new StringBuilder();
+			sb.Append(index + "[Lib]\n");
+			sb.Append(this._lib.ToString(index + "\t"));
+
+			return sb.ToString();
+		}
+
+		public override string ToString()
+		{
+			return this.ToString("");
+		}
+	}
+}
