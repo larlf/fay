fbb63db03900ba1442b35703088815b3639e8c30
Larlf.Wang <larlf.wang@gmail.com>
2018-09-26 23:54:25 +0800

    Add StringBuilder

--- cpp/src/mirror_util_string.h
+++ cpp/src/mirror_util_string.h
@@ -1,12 +1,15 @@
 ﻿#pragma once
 
 #include <iostream>
+#include <string>
 
 namespace mirror
 {
 	namespace util
 	{
-		//字符串相关的公用方法
+		/**
+		 * 字符串相关的公用方法
+		 */
 		class StringUtils
 		{
 		public:
@@ -17,6 +20,99 @@ namespace mirror
 			* @param toCode 目标编码
 			*/
 			static std::string Encoding(const std::string &source, const std::string &fromCode, const std::string &toCode);
+		};
+
+		/**
+		 * 字符串构造类
+		 */
+		class StringBuilder
+		{
+		private:
+			std::string text;
+			int indent;
+			std::string indentStr;
+			bool isNewline;
+
+			void checkNewline()
+			{
+				if (this->isNewline)
+				{
+					text.append(this->indentStr);
+					this->isNewline = false;
+				}
+			}
+
+		public:
+			StringBuilder() : indent(0), isNewline(true)
+			{
+			}
+
+			StringBuilder *add(const char *str)
+			{
+				this->checkNewline();
+				text.append(str);
+				return this;
+			}
+
+			StringBuilder *add(const std::string &str)
+			{
+				this->checkNewline();
+				text.append(str);
+				return this;
+			}
+
+			StringBuilder *add(const char c)
+			{
+				this->checkNewline();
+				text.append(&c, 1);
+				return this;
+			}
+
+			StringBuilder *add(int v)
+			{
+				this->checkNewline();
+				text.append(std::to_string(v));
+				return this;
+			}
+
+			StringBuilder *endl()
+			{
+				text.append("\n");
+				this->isNewline = true;
+				return this;
+			}
+
+			StringBuilder *indentAdd()
+			{
+				this->indent++;
+				this->indentStr.clear();
+				for (auto i = 0; i < this->indent; ++i)
+					this->indentStr.append("  ");
+				return this;
+			}
+
+			StringBuilder *indentSub()
+			{
+				this->indent--;
+				this->indentStr.clear();
+				for (auto i = 0; i < this->indent; ++i)
+					this->indentStr.append("  ");
+				return this;
+			}
+
+			StringBuilder *clear()
+			{
+				this->text.clear();
+				this->indentStr.clear();
+				this->indent = 0;
+				this->isNewline = false;
+				return this;
+			}
+
+			std::string toString()
+			{
+				return this->text;
+			}
 		};
 	}
 }
