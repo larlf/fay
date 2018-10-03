3555fdb4a1f21ae205ccd52074f9675491d18c31
Larlf.Wang <larlf.wang@gmail.com>
2018-10-03 23:01:14 +0800

    Add TokenStack

--- cpp/src/fay_token_stack.cpp
+++ cpp/src/fay_token_stack.cpp
@@ -7,11 +7,57 @@ PTR(Token) fay::TokenStack::EndToken(new Token());
 
 PTR(Token) fay::TokenStack::now()
 {
-	if (this->_tokens == nullptr)
-		return this->EndToken;
-
-	if (this->_pos < this->_tokens->size())
+	if (this->_pos>=0 && this->_pos < this->_tokens->size())
 		return (*this->_tokens)[this->_pos];
 
 	return this->EndToken;
+}
+
+PTR(Token) fay::TokenStack::before()
+{
+	if (this->_pos >= 1 && this->_pos < this->_tokens->size() + 1)
+		return (*this->_tokens)[this->_pos -1];
+
+	return this->EndToken;
+}
+
+PTR(Token) fay::TokenStack::after()
+{
+	if (this->_pos >= -1 && this->_pos < this->_tokens->size() - 1)
+		return (*this->_tokens)[this->_pos + 1];
+
+	return this->EndToken;
+}
+
+PTR(Token) fay::TokenStack::next()
+{
+	if (this->_pos < this->_tokens->size())
+		this->_pos++;
+
+	//跳过注释
+	if (this->now()->is(TokenType::Comment))
+		this->next();
+
+	return this->now();
+}
+
+PTR(Token) fay::TokenStack::move()
+{
+	auto token = this->now();
+	this->next();
+	return token;
+}
+
+PTR(Token) fay::TokenStack::findNextToken(const std::vector<TokenType>& types)
+{
+	for (auto i = this->_pos; i < this->_tokens->size(); ++i)
+	{
+		for (auto j = 0; j < types.size(); ++j)
+		{
+			if ((*this->_tokens)[i]->type() == types[j])
+				return (*this->_tokens)[i];
+		}
+	}
+
+	return nullptr;
 }
--- cpp/src/fay_token_stack.h
+++ cpp/src/fay_token_stack.h
@@ -4,6 +4,7 @@
 
 namespace fay
 {
+	//用于进行语法解析处理时的数据结构
 	class TokenStack
 	{
 	private:
@@ -17,8 +18,19 @@ namespace fay
 		TokenStack(PTR(std::vector<PTR(Token)>) tokens)
 			: _tokens(tokens) {}
 
+		//当前的位置
 		int pos() { return this->_pos; }
-
+		//返回当前的Token
 		PTR(Token) now();
+		//返回前一个Token
+		PTR(Token) before();
+		//返回后一个Token
+		PTR(Token) after();
+		//移动并返回下一个Token
+		PTR(Token) next();
+		//返回当前Token并移动到下一个
+		PTR(Token) move();
+		//查找符合要求的下一个关键字
+		PTR(Token) findNextToken(const std::vector<TokenType> &types);
 	};
 }
