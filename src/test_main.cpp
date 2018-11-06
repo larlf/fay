11b3565d4a3c517944e891d7b6e126aacd89a34c
Larlf.Wang <larlf.wang@gmail.com>
2018-11-07 00:08:08 +0800

    Add gtest

--- src/test_main.cpp
+++ src/test_main.cpp
@@ -6,28 +6,37 @@
 #include <mirror_utils_log.h>
 #include <fay_lexer.h>
 #include <fay_parser.h>
+#include <gtest/gtest.h>
 
-void main()
+int main(int argc, char** argv)
 {
-	try
-	{
-		//test::UtilsTest::testLog();
-		//test::UtilsTest::testString();
-		//test::UtilsTest::testTrace();
 
-		test::FayTests::Init();
-		test::FayTests::TestLexer();
-		//test::FayTests::Test1();
-		//test::FayTests::TestRTTI();
-		//test::FayTests::TestInternalFun();
-	}
-	catch (fay::FayCompileException &e)
-	{
-		PRINT("Error : " << e.what());
-		PRINT("Line:" << e.line() << " Col:" << e.col() << " @ " << e.file()->filename());
-		PRINT(e.source());
-		PRINT(e.trace());
-	}
+	VLDDisable();
+	testing::FLAGS_gtest_death_test_style = "fast";
+
+	testing::InitGoogleTest(&argc, argv);
+
+	return RUN_ALL_TESTS();
+
+	//try
+	//{
+	//	//test::UtilsTest::testLog();
+	//	//test::UtilsTest::testString();
+	//	//test::UtilsTest::testTrace();
+
+	//	test::FayTests::Init();
+	//	test::FayTests::TestLexer();
+	//	//test::FayTests::Test1();
+	//	//test::FayTests::TestRTTI();
+	//	//test::FayTests::TestInternalFun();
+	//}
+	//catch (fay::FayCompileException &e)
+	//{
+	//	PRINT("Error : " << e.what());
+	//	PRINT("Line:" << e.line() << " Col:" << e.col() << " @ " << e.file()->filename());
+	//	PRINT(e.source());
+	//	PRINT(e.trace());
+	//}
 
 }
 
